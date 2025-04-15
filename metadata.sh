6909866880b4f4974ff388c00e3281fc6260a848
Tomasz Wędrychowicz <wendryc1963@gmail.com>
2025-04-15 16:54:50 +0200

    Add operation system to file

--- metadata.sh
+++ metadata.sh
@@ -10,7 +10,12 @@ PRIVATEIP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 SG=$(curl http://169.254.169.254/latest/meta-data/security-groups)
 
+OS=$(cat /etc/os-release | grep "^NAME=" | cut -d "=" -f2 | tr -d '"')
+
+OSV=$(cat /etc/os-release | grep "^VERSION_ID=" | cut -d "=" -f2 | tr -d '"')
+
 echo """Instance ID: $INSTANCEID
 Public IP: $PUBLICIP
 Private IP: $PRIVATEIP 
-Security Groups: $SG""" > metadata.txt
+Security Groups: $SG
+Operating System: $OS $OSV""" > metadata.txt
